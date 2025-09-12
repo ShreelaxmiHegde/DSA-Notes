273731f20c311ec2c87721b7703adcd8d20d9c23
ShreelaxmiHegde <shreelaxmihegde9@gmail.com>
2025-09-12 09:13:47 +0530

    added another q

---
+++ Stack/394. Decode String.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <string>
+using namespace std;
+
+
+
+int main() {
+    string str = "abc";
+    string newStr = str+str;
+    cout << newStr;
+    
+    return 0;
+}
